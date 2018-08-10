f4087c543bd51c14d7570e4a6be23619af84001f
CHIK SUM <chiksum.wong@gmail.com>
2018-08-10 23:52:09 +0800

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :keyword_mappings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/tomcat/webhook', to: 'tomcat#webhook'
+
+  root to: 'home#index'
 end
