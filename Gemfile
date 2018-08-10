92b2e8703dec61eb9222b41702ca6aa68e861e50
CHIK SUM <chiksum.wong@gmail.com>
2018-08-10 21:44:58 +0800

    Added Backend

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #line
 gem 'line-bot-api'
+
+#devise
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :keyword_mappings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/tomcat/webhook', to: 'tomcat#webhook'
 end
